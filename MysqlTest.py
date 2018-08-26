4b06d4338dbfb6bbc3048024ff87b6780d55eb82
dsujatha <42560098+dsujatha@users.noreply.github.com>
2018-08-27 00:05:31 +0530

    Add files via upload

--- MysqlTest.py
+++ MysqlTest.py
@@ -1,33 +1,21 @@
+import sys
 import MySQLdb
-import xlwt
 
-
-#Instantiates a workbook and sheet to write the query results to
-workbook = xlwt.Workbook()
-sheet = workbook.add_sheet("Sheet1") # name of the worksheet
+import pandas as pd
 
 
 #Database connection
 db = MySQLdb.connect(host="localhost", # your host, usually localhost
 user="root", # your username
-passwd="test@123", # your password
+passwd="Mind@123", # your password
 db="northwind") # name of the data base
 
+sql = "SELECT * FROM orders"
 
-# you must create a Cursor object. It will let
-# you execute all the queries you need
-cur = db.cursor()
+df = pd.read_sql(sql, db)
 
+#print df.head()
 
-# Use all the SQL you like
-cur.execute("SELECT * FROM YOUR_TABLE_NAME")
-
-rowNum = 0 #keep track of rows
-colNum = 0 #keep track of columns
-
-
-# print all the cells of the row to excel sheet
-for row in cur.fetchall() :
-sheet.write(rowNum, colNum, row) # row, column, value
-rowNum = rowNum + 1
-colNum = colNum + 1
+#Export to CSV
+df.to_excel('orders.xlsx', index=False)
+print('Done')
